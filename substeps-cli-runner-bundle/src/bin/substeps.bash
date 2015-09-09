bfe7457d05e6e209bfec9cf4bf139fcea3d88412
James Bacon <jbacon@technophobia.com>
2015-09-09 13:29:43 +0100

    Change to the shell script, that 'should' allow it to know where it is, moved to bash than simple shell support.

---
+++ substeps-cli-runner-bundle/src/bin/substeps.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Enable running script from any location
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Just run it now, adding properties to the classpath
+java -classpath "properties/*:lib/*" "co.uk.baconi.substeps.cli.MainRunner" "${@}"
--- substeps-cli-runner-bundle/src/bin/substeps.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-java -classpath "properties/*:lib/*" "co.uk.baconi.substeps.cli.MainRunner" "${@}"
